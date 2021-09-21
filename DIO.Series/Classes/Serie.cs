4370b26216c62673886ec2906dcc7815329267ca
vinicioslop <vinicioslop7@hotmail.com>
2021-09-21 15:36:14 -0300

    Finaliza o projeto com as alterações feitas em aula

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace DIO.Series
+{
+    // Atributos
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+
+        // Metodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // link da documentacao do Environment.NewLine
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            retorno += "\nExcluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
